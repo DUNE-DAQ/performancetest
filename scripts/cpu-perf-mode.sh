a8bc2f280ec373208ab97065382ceefc8c9526ae
roland-sipos <sipos.roland.cb@gmail.com>
2022-10-10 11:00:41 +0200

    toggle idle states and frequency of cpu cores

---
+++ scripts/cpu-perf-mode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Disabling Idle-State level 6-2 on all cores..."
+cpupower -c all idle-set -d 6
+cpupower -c all idle-set -d 5
+cpupower -c all idle-set -d 4
+cpupower -c all idle-set -d 3
+cpupower -c all idle-set -d 2
+cpupower idle-info
+
+echo " "
+echo "Set frequency governor to performance (if available)..."
+cpupower frequency-set --governor performance
+cpupower frequency-info
+
+echo " "
+echo "Set performance bias to max performance..."
+cpupower -c all set --perf-bias 0
+cpupower -c all info
+
+echo " "
+echo "Monitor current Idle-States..."
+cpupower monitor
+
+echo " "
+echo "#########################################"
+echo "# Cross-check current idle states above #"
+echo "#      All cores should idle in C1      #"
+echo "#########################################"
+
