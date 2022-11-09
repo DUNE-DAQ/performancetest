0168b2df7cee692c85baac7645680ed5e18dd2c3
Matthew Man <centos@superdune.physics.utoronto.ca>
2022-11-09 17:16:40 -0500

    added snb recording test scripts

---
+++ tests/snb_write_gen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: ./link_scaling_gen.sh <host_address> <pin_file> <output_path>"
+  echo "host_address is the address to host all processes except readout (run locally)"
+  echo "pin_file is the full path to the cpu pinning config file"
+  echo "output path for raw recording"
+  exit 2
+fi
+
+host_address=$1
+pin_file=$2
+output_path=$3
+
+for i in {1..24}
+do
+        daqconf_multiru_gen -d frames.bin -o . -n $i --thread-pinning-file $pin_file --host-df $host_address --host-dfo $host_address --host-trigger $host_address --host-hsi $host_address --enable-software-tpg --enable-raw-recording --raw-recording-output-dir $ouput_path snb_write_n${i}
+
+done
+
+
