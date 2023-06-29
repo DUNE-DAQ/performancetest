0b3d7f5bb069a8a93f4ccc8028c3ad7e192068f7
Danaisis Vargas Oliva <dvargas@np02-srv-004.cern.ch>
2023-06-29 08:11:54 +0200

    update of the wib ethernet daqconfs, cpupins, and readoutmaps files

--- tests/config_gen.sh
+++ tests/config_gen.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-  echo "Usage: ./config_gen.sh <daqconf_file> <hwmap_file> <test_name>"
+if [ $# -ne 4 ]; then
+  echo "Usage: ./config_gen.sh <path> <daqconf_file> <hwmap_file> <test_name>"
+  ecjo "path to performancetest"
   exit 2
 fi
 
-daqconf_file=$1
-hwmap_file=$2
-test_name=$3
+path=$1
+daqconf_file=$2
+hwmap_file=$3
+test_name=$4
 
-daqconf_multiru_gen -c ./../dunedaq-v4.0.0/sourcecode/performancetest/daqconfs/$daqconf_file --hardware-map-file ./../dunedaq-v4.0.0/sourcecode/performancetest/hwmaps/$hwmap_file $test_name
+daqconf_multiru_gen -c ./$path/daqconfs/$daqconf_file --hardware-map-file ./$path/hwmaps/$hwmap_file $test_name
