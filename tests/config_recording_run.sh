3e730c83d08706517eb93027e3ae158f9288adf4
Danaisis Vargas Oliva <dvargas@np02-srv-004.cern.ch>
2023-07-20 02:48:50 +0200

    automatic run of tests

--- tests/config_recording_run.sh
+++ tests/config_recording_run.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 if [ $# -ne 4 ]; then
-  echo "Usage: ./config_recording_run.sh <test_name> <envir_name> <run_number> <path>"
+  echo "Usage: ./config_recording_run.sh <test_name> <envir_name> <run_num_init> <path>"
   echo "path to performancetest"
   exit 2
 fi
 
-test_name=$1
+test_names=$1
 envir_name=$2
-run_num=$3
+run_num_init=$3
 path=$4
 
-iostat -m -t 1 120 > iostat_$test_name.txt &
-nanorc $test_name $envir_name boot conf start_run $run_num  expert_command --timeout 10 $test_name/$test_name/runp02srv00* $path/tests/record-cmd.json wait 600 stop_run shutdown
+#test_names=('us-east-2a' 'us-west-1a' 'eu-central-1a')
 
-# move log files
-mkdir RunConf_$run_num/logs
-mv log_*.txt RunConf_$run_num/logs
-grep -R "ERROR" RunConf_$run_num/logs >> error_summary.txt
+for test_name_i in "${test_names[@]}"
+do
+    run_num=$(($run_num_init+(counter++)))
+    echo "$test_name_i"
+    iostat -m -t 1 120 > iostat_$test_name_i.txt &
+    nanorc $test_name_i $envir_name boot conf start_run $run_num  expert_command --timeout 10 $test_name_i/$test_name_i/runp02srv00* $path/tests/record-cmd.json wait 600 stop_run shutdown
 
-# move output files
-mkdir RunConf_$run_num/output
-mv *.hdf5 RunConf_$run_num/output
-mv logbook_*.txt RunConf_$run_num/output
-mv error_summary.txt RunConf_$run_num/output
-mv iostat_*.txt RunConf_$run_num/output
 
-echo "wait for 120s for resources to return to baseline"
-sleep 120
+    # move log files
+    mkdir RunConf_$run_num/logs
+    mv log_*.txt RunConf_$run_num/logs
+    grep -R "ERROR" RunConf_$run_num/logs >> error_summary.txt
+
+    # move output files
+    mkdir RunConf_$run_num/output
+    mv *.hdf5 RunConf_$run_num/output
+    mv logbook_*.txt RunConf_$run_num/output
+    mv error_summary.txt RunConf_$run_num/output
+    mv iostat_*.txt RunConf_$run_num/output
+
+    echo "wait for 120s for resources to return to baseline"
+    sleep 120
+done
--- tests/config_run.sh
+++ tests/config_run.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-  echo "Usage: ./config_recording_run.sh <test_name> <envir_name> <run_number>"
+  echo "Usage: ./config_recording_run.sh <test_names> <envir_name> <run_number_init>"
   exit 2
 fi
 
-test_name=$1
+test_names=$1
 envir_name=$2
-run_num=$3
+run_num_init=$3
 
-iostat -m -t 1 120 > iostat_$test_name.txt &
-nanorc $test_name $envir_name boot conf start_run $run_num wait 600 stop_run shutdown
+#test_names=('us-east-2a' 'us-west-1a' 'eu-central-1a')
 
-# move log files
-mkdir RunConf_$run_num/logs
-mv log_*.txt RunConf_$run_num/logs
-grep -R "ERROR" RunConf_$run_num/logs >> error_summary.txt
+for test_name_i in "${test_names[@]}"
+do
+    run_num=$(($run_num_init+(counter++)))
+    echo "$test_name_i"
+    iostat -m -t 1 120 > iostat_$test_name_i.txt &
+    nanorc $test_name_i $envir_name boot conf start_run $run_num wait 600 stop_run shutdown
 
-# move output files
-mkdir RunConf_$run_num/output
-mv *.hdf5 RunConf_$run_num/output
-mv logbook_*.txt RunConf_$run_num/output
-mv error_summary.txt RunConf_$run_num/output
-mv iostat_*.txt RunConf_$run_num/output
+    # move log files
+    mkdir RunConf_$run_num/logs
+    mv log_*.txt RunConf_$run_num/logs
+    grep -R "ERROR" RunConf_$run_num/logs >> error_summary.txt
 
-echo "wait for 120s for resources to return to baseline"
-sleep 120
+    # move output files
+    mkdir RunConf_$run_num/output
+    mv *.hdf5 RunConf_$run_num/output
+    mv logbook_*.txt RunConf_$run_num/output
+    mv error_summary.txt RunConf_$run_num/output
+    mv iostat_*.txt RunConf_$run_num/output
+
+    echo "wait for 120s for resources to return to baseline"
+    sleep 120
+done
