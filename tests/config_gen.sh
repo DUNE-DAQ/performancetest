2234c51a4b3e5f5cdda5d97b0a285deccd90f0d8
Danaisis Vargas Oliva <dvargas@np02-srv-003.cern.ch>
2023-07-20 16:39:43 +0200

    automatic run of tests

--- tests/config_gen.sh
+++ tests/config_gen.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $# -ne 4 ]; then
-  echo "Usage: ./config_gen.sh <path> <daqconf_file> <hwmap_file> <test_name>"
-  echo "path to performancetest"
+  echo "Usage: ./config_gen.sh <path> <daqconf_file> <readoutmap_file> <test_name>"
+  echo "<path>: path to performancetest"
   exit 2
 fi
 
 path=$1
 daqconf_file=$2
-hwmap_file=$3
+readoutmap_file=$3
 test_name=$4
 
-daqconf_multiru_gen -c ./$path/daqconfs/$daqconf_file --hardware-map-file ./$path/hwmaps/$hwmap_file $test_name
+daqconf_multiru_gen -c ./$path/daqconfs/$daqconf_file --detector-readout-map-file ./$path/readoutmaps/$readoutmap_file $test_name
--- tests/config_run.sh
+++ tests/config_run.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-  echo "Usage: ./config_recording_run.sh <test_names> <envir_name> <run_number_init>"
+if [ $# -ne 4 ]; then
+  echo "Usage: ./config_run.sh <path> <envir_name> <run_num_init> <test_type>"
+  echo "<path>: path to performancetest"
+  echo "<test_type>: options amd, intel, amd_recording_wib2, amd_recording_wibeth, intel_recording_wib2, intel_recording_wibeth"
   exit 2
 fi
 
-test_names=$1
+path=$1
 envir_name=$2
 run_num_init=$3
+test_type=$4
 
-#test_names=('us-east-2a' 'us-west-1a' 'eu-central-1a')
+if [ "$test_type" = "amd" ]; then
+    test_names=("perf_12link_amd_node0" "perf_12link_amd_node1" "perf_12link_amd_node1_swtpg'  'perf_24link_amd_node0" "perf_24link_amd_node1" "perf_24link_amd_node1_swtpg" "perf_eth_48link_amd_node0" "perf_eth_48link_amd_node1" "perf_eth_48link_amd_node1_swtpg" "perf_eth_96link_amd_node0" "perf_eth_96link_amd_node1" "perf_eth_96link_amd_node1_swtpg")
+elif [ "$test_type" = "intel" ]; then
+    test_names=("perf_12link_intel_node0" "perf_12link_intel_node1" "perf_12link_intel_node1_swtpg" "perf_24link_intel_node0" "perf_24link_intel_node1" "perf_24link_intel_node1_swtpg" "perf_eth_48link_intel_node0" "perf_eth_48link_intel_node1" "perf_eth_48link_intel_node1_swtpg" "perf_eth_96link_intel_node0" "perf_eth_96link_intel_node1" "perf_eth_96link_intel_node1_swtpg")
+elif [ "$test_type" = "amd_recording_wib2" ]; then
+    test_names=("perf_12link_amd_node1_recording" "perf_12link_amd_node1_recording_swtpg" "perf_24link_amd_node1_recording" "perf_24link_amd_node1_recording_swtpg")
+elif [ "$test_type" = "amd_recording_wibeth" ]; then
+    test_names=("perf_eth_48link_amd_node1_recording" "perf_eth_48link_amd_node1_recording_swtpg" "perf_eth_96link_amd_node1_recording" "perf_eth_96link_amd_node1_recording_swtpg")
+elif [ "$test_type" = "intel_recording_wib2" ]; then
+    test_names=("perf_12link_intel_node1_recording" "perf_12link_intel_node1_recording_swtpg" "perf_24link_intel_node1_recording" "perf_24link_intel_node1_recording_swtpg")
+else
+    test_names=("perf_eth_48link_intel_node1_recording" "perf_eth_48link_intel_node1_recording_swtpg" "perf_eth_96link_intel_node1_recording" "perf_eth_96link_intel_node1_recording_swtpg")
+fi
 
 for test_name_i in "${test_names[@]}"
 do
     run_num=$(($run_num_init+(counter++)))
-    echo "$test_name_i"
+    echo "running tests $test_name_i"
     iostat -m -t 1 120 > iostat_$test_name_i.txt &
-    nanorc $test_name_i $envir_name boot conf start_run $run_num wait 600 stop_run shutdown
-
+    if [ "$test_type" = "amd" ] || [ "$test_type" = "intel" ]; then
+	nanorc $test_name_i $envir_name boot conf start_run $run_num wait 600 stop_run shutdown
+    elif [ "$test_type" = "amd_recording_wib2" ]; then
+        nanorc $test_name_i $envir_name boot conf start_run $run_num expert_command --timeout 10 $test_name_i/$test_name_i/runp02srv004flx1 $path/tests/record-cmd.json wait 600 stop_run shutdown
+    elif [ "$test_type" = "amd_recording_wibeth" ]; then
+        nanorc $test_name_i $envir_name boot conf start_run $run_num expert_command --timeout 10 $test_name_i/$test_name_i/runp02srv004eth1 $path/tests/record-cmd.json wait 600 stop_run shutdown
+    elif [ "$test_type" = "intel_recording_wib2" ]; then
+        nanorc $test_name_i $envir_name boot conf start_run $run_num expert_command --timeout 10 $test_name_i/$test_name_i/runp02srv003flx1 $path/tests/record-cmd.json wait 600 stop_run shutdown
+    else
+        nanorc $test_name_i $envir_name boot conf start_run $run_num expert_command --timeout 10 $test_name_i/$test_name_i/runp02srv003eth1 $path/tests/record-cmd.json wait 600 stop_run shutdown     
+    fi
+    
     # move log files
     mkdir RunConf_$run_num/logs
     mv log_*.txt RunConf_$run_num/logs
