5fcc4b3e462862883e7cbcfbb27e52ab43157912
Danaisis Vargas Oliva <dvargas@np04-srv-003.cern.ch>
2023-09-28 16:12:56 +0200

    ready to merged

--- tests/generating_all_configs.sh
+++ tests/generating_all_configs.sh
@@ -1,8 +1,8 @@
-#./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling NFD23_09_21 np04srv003
-#./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling_swtpg NFD23_09_21 np04srv003
+./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling NFD23_09_21 np04srv003
+./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling_swtpg NFD23_09_21 np04srv003
 ./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling_recording NFD23_09_21 np04srv003
-#./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling_recording_swtpg NFD23_09_21 np04srv003
-#./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling NFD23_09_21 np04srv003
-#./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling_swtpg NFD23_09_21 np04srv003
+./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv003 0 eth stream_scaling_recording_swtpg NFD23_09_21 np04srv003
+./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling NFD23_09_21 np04srv003
+./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling_swtpg NFD23_09_21 np04srv003
 ./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling_recording NFD23_09_21 np04srv003
-#./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling_recording_swtpg NFD23_09_21 np04srv003
+./config_gen.sh /nfs/home/dvargas/DUNE-SW/dunedaq-NFD23-09-21/sourcecode/performancetest np02srv004 0 eth stream_scaling_recording_swtpg NFD23_09_21 np04srv003
