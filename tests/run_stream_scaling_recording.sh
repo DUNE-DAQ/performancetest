bf41d4d810869385e1dd7279d01065395d84e16a
Matthew Gar Jun Man <mman@np02-srv-003.cern.ch>
2023-09-29 15:16:06 +0200

    added (timed) fstrim to performance test

--- tests/run_stream_scaling_recording.sh
+++ tests/run_stream_scaling_recording.sh
@@ -17,11 +17,15 @@ cd $test_name
 for i in {8..48..8}
 do
 	# disk throughput
-    iostat -m -t 1 120 > iostat_${i}.txt &
+	iostat -m -t 1 120 > iostat_${i}.txt &
 
 	echo "Scaling to ${i} streams"
 	run_num_increment=${i}/8
 	run_num=$(($run_num_init + $run_num_increment -1))
+
+	# trim disks
+        { time sudo fstrim -v /mnt/nvm_raid0; } 2> fstrim_raid0_${run_num}.time
+        { time sudo fstrim -v /mnt/nvm_raid1; } 2> fstrim_raid1_${run_num}.time
 
 	nanorc ${i} $envir_name boot conf start_run $run_num expert_command --timeout 10 ${i}/${i}/ru${server}eth${NUMA_node_num} ../record-cmd.json wait 600 stop_run shutdown
 
@@ -36,6 +40,8 @@ do
 	mv logbook_*.txt RunConf_$run_num/output
 	mv error_summary.txt RunConf_$run_num/output
 	mv iostat_*.txt RunConf_$run_num/output
+	mv fstrim_raid0_${run_num}.time RunConf_$run_num/output
+        mv fstrim_raid1_${run_num}.time RunConf_$run_num/output
 
 	echo "wait for 120s for resources to return to baseline"
 	sleep 120
