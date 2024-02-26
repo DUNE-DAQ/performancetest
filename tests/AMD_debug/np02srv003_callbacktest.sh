dddb8dc6a5a4a8b2d81c1967cc483a43e31c9e76
Matthew Gar Jun Man <mman@np02-srv-004.cern.ch>
2024-02-26 16:13:38 +0100

    added callback tests using mixed pinning config

--- tests/AMD_debug/np02srv003_callbacktest.sh
+++ tests/AMD_debug/np02srv003_callbacktest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # need to source the environment file first
 
-if [ $# -ne 2 ]; then
-  echo "Usage: ./AMD_callback_test.sh <output_path> <cpupin_file>"
+if [ $# -ne 3 ]; then
+  echo "Usage: ./AMD_callback_test.sh <output_path> <cpupin_file> <test_name>"
   echo "Ex: ./AMD_callback_test.sh callbacktest_results/ cpupins/cpupin-eth-mockdlh-np02srv003-1.json"
   exit 2
 fi
@@ -11,14 +11,15 @@ fi
 test_time=90
 output_path=$1
 cpupin_file=$2
+test_name=$3
 
 # NUMA0 cores
 cpu_affinity=("1-111:2")
 
 ## Callbacks
 echo "> consumer_mode : callback"
-test_name=v4_2_1-np02srv004-1-eth-callback_Intel
-sar -P ALL 5 18 >> $output_path/core_utilization-${test_name}.csv &
+#test_name=v4_2_1-np02srv003-1-eth-callback_Intel
+sar -P ALL 5 18 >> $output_path/core_utilization-v4_2_1-np02srv003-1-eth-callback_${test_name}.csv &
 		
 taskset -c $cpu_affinity rubberdaq_test_mock_dlh --run_secs $test_time -n 10 --cb &
 sleep 4
@@ -28,8 +29,8 @@ sleep 120
 
 ## Queues
 echo "> consumer_mode : queue"
-test_name=v4_2_1-np02srv004-1-eth-queue_Intel
-sar -P ALL 5 18 >> $output_path/core_utilization-${test_name}.csv &
+#test_name=v4_2_1-np02srv003-1-eth-queue_Intel
+sar -P ALL 5 18 >> $output_path/core_utilization-v4_2_1-np02srv003-1-eth-queue_${test_name}.csv &
 
 taskset -c $cpu_affinity rubberdaq_test_mock_dlh --run_secs $test_time -n 10 --ct &
 sleep 4
--- tests/AMD_debug/np02srv004_callbacktest.sh
+++ tests/AMD_debug/np02srv004_callbacktest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # need to source the environment file first
 
-if [ $# -ne 2 ]; then
-  echo "Usage: ./AMD_callback_test.sh <output_path> <cpupin_file>"
-  echo "Ex: ./AMD_callback_test.sh 60"
+if [ $# -ne 3 ]; then
+  echo "Usage: ./AMD_callback_test.sh <output_path> <cpupin_file> <test_name>"
+  echo "Ex: ./AMD_callback_test.sh callbacktest_results/ ../../cpupins/cpupin-eth-mockdlh_mixed-np02srv004-1.json AMD_mixed"
   exit 2
 fi
 
@@ -11,18 +11,18 @@ fi
 test_time=90
 output_path=$1
 cpupin_file=$2
+test_name=$3
 
 # NUMA0 cores
 cpu_affinity=("64-127,192-255")
 
 ## Callbacks
 echo "> consumer_mode : callback"
-test_name=v4_2_1-np02srv004-1-eth-callback_AMD
 # Start uProf monitoring
-sudo /opt/AMDuProf_4.0-341/bin/AMDuProfPcm -a -s -d 95 -t 250 -m memory,ipc,l1,l2,l3 -A package,ccd -k -o $output_path/uprof-$test_name.csv &
+sudo /opt/AMDuProf_4.0-341/bin/AMDuProfPcm -a -s -d 95 -t 250 -m memory,ipc,l1,l2,l3 -A package,ccd -k -o $output_path/uprof-v4_2_1-np02srv004-1-eth-callback_$test_name.csv &
 
 # cpu core utilization monitoring at 5 second intervals
-sar -P ALL 5 18 >> $output_path/core_utilization-${test_name}.txt &
+sar -P ALL 5 18 >> $output_path/core_utilization-v4_2_1-np02srv004-1-eth-callback_${test_name}.csv &
 
 taskset -c $cpu_affinity rubberdaq_test_mock_dlh --run_secs $test_time -n 10 --cb &
 sleep 4
@@ -32,10 +32,9 @@ sleep 120
 
 ## Queues
 echo "> consumer_mode : queue"
-test_name=v4_2_1-np02srv004-1-eth-queue_AMD
-sudo /opt/AMDuProf_4.0-341/bin/AMDuProfPcm -a -s -d 95 -t 250 -m memory,ipc,l1,l2,l3 -A package,ccd -k -o $output_path/uprof-$test_name.csv &
+sudo /opt/AMDuProf_4.0-341/bin/AMDuProfPcm -a -s -d 95 -t 250 -m memory,ipc,l1,l2,l3 -A package,ccd -k -o $output_path/uprof-v4_2_1-np02srv004-1-eth-queue_$test_name.csv &
 
-sar -P ALL 5 18 >> $output_path/core_utilization-${test_name}.txt &
+sar -P ALL 5 18 >> $output_path/core_utilization-v4_2_1-np02srv004-1-eth-queue_${test_name}.csv &
 
 taskset -c $cpu_affinity rubberdaq_test_mock_dlh --run_secs $test_time -n 10 --ct &
 sleep 4
