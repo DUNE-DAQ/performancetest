8cb40cbcf2891d3f10e28f98f08c2f5efaba26c4
Matthew Gar Jun Man <mman@np02-srv-001.cern.ch>
2024-03-11 16:04:02 +0100

    added start_uprof script compatible with uprof version on np02srv001

---
+++ scripts/start_uprof_np02srv001.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "Usage: ./start_uprof.sh <output_directory> <test_name> <duration in seconds>"
+  exit 2
+fi
+
+output_dir=$1
+test_name=$2
+duration=$3
+
+echo "start uprof monitoring"
+/opt/AMDuProf_4.1-424/bin/AMDuProfPcm -a -s -d $duration -t 2500 -m memory,ipc,l1,l2,l3 -A package -k -o $output_dir/$test_name/uprof-$test_name.csv &
+/opt/AMDuProf_4.1-424/bin/AMDuProfCLI-bin timechart --event power --interval 15000 --duration $duration -o $output_dir/$test_name 
+
+cd $output_dir/$test_name/AMDuProf-SWP-Timechart_*/
+mv timechart.csv ../timechart-$test_name.csv
+
+cd ../
