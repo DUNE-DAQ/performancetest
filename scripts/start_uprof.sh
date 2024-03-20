8bdc211765e49341729578e4e5cadac82c93127f
Danaisis Vargas Oliva <dvargas@np04-srv-031.cern.ch>
2024-03-20 09:05:59 +0100

    adding files for the dualreadout in one server

--- scripts/start_uprof.sh
+++ scripts/start_uprof.sh
@@ -13,8 +13,8 @@ duration=$3
 sar -P ALL 60 75 >> $output_dir/$test_name/core_utilization-${test_name}.csv &
 
 echo "start uprof monitoring"
-/opt/AMDuProf_4.0-341/bin/AMDuProfPcm -a -s -d $duration -t 2500 -m memory,ipc,l1,l2,l3 -A package -k -o $output_dir/$test_name/uprof-${test_name}.csv &
-/opt/AMDuProf_4.0-341/bin/AMDuProfCLI-bin timechart --event power --interval 15000 --duration $duration -o $output_dir/$test_name 
+/opt/AMDuProf_*/bin/AMDuProfPcm -a -s -d $duration -t 2500 -m memory,ipc,l1,l2,l3 -A package -k -o $output_dir/$test_name/uprof-${test_name}.csv &
+/opt/AMDuProf_*/bin/AMDuProfCLI-bin timechart --event power --interval 15000 --duration $duration -o $output_dir/$test_name 
 
 cd $output_dir/$test_name/AMDuProf-SWP-Timechart_*/
 mv timechart.csv ../timechart-${test_name}.csv
--- tests/generating_all_configs.sh
+++ tests/generating_all_configs.sh
@@ -1,18 +1,21 @@
-#------------------- np02srv003 -----------------
 #./config_gen.sh <server_readout> <NUMA_node_num> <data_format> <test> <dunedaq_version>
-#./config_gen.sh np02srv003 1 eth Emu_stream_scaling NFDT_PROD4_240306
-#./config_gen.sh np02srv003 1 eth Emu_stream_scaling_swtpg NFDT_PROD4_240306
-#./config_gen.sh np02srv003 1 eth Emu_stream_scaling_recording NFDT_PROD4_240306
-#./config_gen.sh np02srv003 1 eth Emu_stream_scaling_recording_swtpg NFDT_PROD4_240306
+#------------------- np02srv001 -----------------
+#./config_gen.sh np02srv001 0 eth Emu_stream_scaling NFDT_PROD4_240306
+#./config_gen.sh np02srv001 0 eth Emu_stream_scaling_swtpg NFDT_PROD4_240306
+
+#------------------- np02srv003 -----------------
+#./config_gen.sh np02srv003 0 eth Emu_stream_scaling NFDT_PROD4_240306
+#./config_gen.sh np02srv003 0 eth Emu_stream_scaling_swtpg NFDT_PROD4_240306
+#./config_gen.sh np02srv003 0 eth Emu_stream_scaling_recording NFDT_PROD4_240306
+#./config_gen.sh np02srv003 0 eth Emu_stream_scaling_recording_swtpg NFDT_PROD4_240306
 
 #------------------- np02srv004 -----------------
-#./config_gen.sh <server_readout> <NUMA_node_num> <data_format> <test> <dunedaq_version>
 #./config_gen.sh np02srv004 0 eth Emu_stream_scaling NFDT_PROD4_240306
 #./config_gen.sh np02srv004 0 eth Emu_stream_scaling_swtpg NFDT_PROD4_240306
 #./config_gen.sh np02srv004 0 eth Emu_stream_scaling_recording NFDT_PROD4_240306
 #./config_gen.sh np02srv004 0 eth Emu_stream_scaling_recording_swtpg NFDT_PROD4_240306
+fddaqconf_gen -c ../daqconfs/daqconf-eth-Emu_stream_scaling-np02srv004-01.json --detector-readout-map-file ../readoutmaps/RM-np02srv004-01-streams96.json NFDT_PROD4_240306-np02srv004-01-eth-Emu_96_streams
+fddaqconf_gen -c ../daqconfs/daqconf-eth-Emu_stream_scaling_swtpg-np02srv004-01.json --detector-readout-map-file ../readoutmaps/RM-np02srv004-01-streams96.json NFDT_PROD4_240306-np02srv004-01-eth-Emu_96_streams_swtpg
+fddaqconf_gen -c ../daqconfs/daqconf-eth-Emu_stream_scaling-np02srv004-01.json --detector-readout-map-file ../readoutmaps/RM-np02srv004-01-streams80.json NFDT_PROD4_240306-np02srv004-01-eth-Emu_80_streams
+fddaqconf_gen -c ../daqconfs/daqconf-eth-Emu_stream_scaling_swtpg-np02srv004-01.json --detector-readout-map-file ../readoutmaps/RM-np02srv004-01-streams80.json NFDT_PROD4_240306-np02srv004-01-eth-Emu_80_streams_swtpg
 
-#------------------- np02srv004 -----------------
-#./config_gen.sh <server_readout> <NUMA_node_num> <data_format> <test> <dunedaq_version>
-./config_gen.sh np02srv001 0 eth Emu_stream_scaling NFDT_PROD4_240306
-#./config_gen.sh np02srv001 0 eth Emu_stream_scaling_swtpg NFDT_PROD4_240306
