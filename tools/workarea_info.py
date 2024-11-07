bedb2f4f9aec29a8511e9fc92d0fedba1fc9f05e
ShyamB97 <bhuller.shyam@gmail.com>
2024-11-07 12:58:15 +0100

    add option to check for ehn1-daqconfig

--- tools/workarea_info.py
+++ tools/workarea_info.py
@@ -19,6 +19,18 @@ import utils
 
 from rich import print
 
+printout = False
+
+def verbprint(i : any):
+    """ controlled printout.
+
+    Args:
+        i (any): item to print.
+    """
+    if printout is True: print(i)
+    return
+
+
 @contextlib.contextmanager
 def chdir(dire : str):
     """ Switch directories, then back to cwd.
@@ -34,6 +46,21 @@ def chdir(dire : str):
         os.chdir(cwd)
 
 
+def get_repo_info(repo : str) -> list[str]:
+    """ Get the branch name and commit for a github directory.
+
+    Args:
+        repo (str): Path to github repo.
+
+    Returns:
+        list: branch name, short commit hash.
+    """
+    with chdir(repo):
+        result = subprocess.Popen("echo -n \"$(git rev-parse --abbrev-ref HEAD),$( git rev-parse --short HEAD )\"", shell = True, stdout = subprocess.PIPE)
+
+        return result.communicate()[0].decode().split(",")
+
+
 def check_repos(dire : str) -> dict[list]:
     """ Check the branch and commit hash of a github repo.
 
@@ -46,27 +73,56 @@ def check_repos(dire : str) -> dict[list]:
     repo_info = {}
 
     for repo in filter(os.path.isdir, [os.path.join(dire, i) for i in os.listdir(dire)]):
-        with chdir(repo):
-            result = subprocess.Popen("echo -n \"$( git rev-parse --abbrev-ref HEAD ),$( git rev-parse --short HEAD )\"", shell = True, stdout = subprocess.PIPE)
-
-            repo_info[repo.split("/")[-1]] = result.communicate()[0].decode().split(",")
+        repo_info[repo.split("/")[-1]] = get_repo_info(repo)
 
     return repo_info
 
 
-def get_info(path : str, printout : bool) -> dict[str]:
+def check_configs(dire : str) -> tabulate.JupyterHTMLStr | None:
+    """ Check workarea for ehn1-configurations, and return information about the repo.
+
+    Args:
+        dire (str): dunedaq directory.
+
+    Returns:
+        tabulate.JupyterHTMLStr | None: HTML table of repo info.
+    """
+    ehn1_daqconf_path = utils.search_data_file("ehn1-daqconfigs", dire)
+
+    if ehn1_daqconf_path > 0:
+        info = get_repo_info(ehn1_daqconf_path[0]) # should only have one
+        html_table = make_repo_table({"ehn1-daqconfigs" : info})
+        return html_table
+    else:
+        verbprint("no ehn1-daqconfig area was found.")
+        return
+
+
+def make_repo_table(repos : dict) -> tabulate.JupyterHTMLStr:
+    """ Compile information form github repos into a table
+
+    Args:
+        repos (dict): Dictionary where key is repo name and value is output from get_repo_info.
+
+    Returns:
+        tabulate.JupyterHTMLStr: HTML table of repo infos.
+    """
+    table_headers = ["repo", "branch name", "commit"]
+    tab_info = [[k,] + v for k,v in repos.items()]
+    table = tabulate.tabulate(tab_info, tablefmt = "html", headers = table_headers)
+    verbprint(tabulate.tabulate(tab_info, tablefmt = "fancy", headers = table_headers))
+    return table
+
+
+def get_info(path : str) -> dict[str]:
     """ get information about a dunedaq repository and return html formatted tables.
 
     Args:
         path (str): dunedaq directory
-        printout (bool): printout the information.
 
     Returns:
         dict[str]: html tables of the information.
     """
-    def verbprint(i : any):
-        if printout is True: print(i)
-        return
 
     # check this is valid dunedaq directory
     ls = os.listdir(path)
@@ -99,19 +155,20 @@ def get_info(path : str, printout : bool) -> dict[str]:
     html_tables = {"release_info" : html_table_info}
 
     # get the local and remote repos
-    table_headers = ["repo", "branch name", "commit"]
-
     for src, t in zip([env_vars["DUNE_DAQ_RELEASE_SOURCE"], path + "/sourcecode/"], ["release", "local"]):
         repos = check_repos(src)
         verbprint(f"{t} repositories:")
-        tab_info = [[k,] + v for k,v in repos.items()]
-        html_tables["t"] = tabulate.tabulate(tab_info, tablefmt = "html", headers = table_headers)
-        verbprint(tabulate.tabulate(tab_info, tablefmt = "fancy", headers = table_headers))
+        html_tables[t] = make_repo_table(repos)
+
+    html_tables["configuration"] = check_configs(path)
+
     return html_tables
 
 
 def main(args):
-    get_info(args.path, True)
+    global printout
+    printout = True
+    get_info(args.path)
     return
 
 if __name__ == "__main__":
