36c1376df533ef2934ac2647406e921ae379eab4
ShyamB97 <bhuller.shyam@gmail.com>
2024-11-13 18:33:20 +0100

    harvest all modes for the  cpu times

--- src/harvester.py
+++ src/harvester.py
@@ -348,7 +348,7 @@ def extract_node_exporter_data(dashboard_info : dict[str], run_number : int, hos
     """
     query_dict = {
         "CPU Usage (%)" : f"100 * (1 - irate(node_cpu_seconds_total{{nodename=\"{host}\", mode=\"idle\"}}[10m]))",
-        "CPU Idle (s)" : f"node_cpu_seconds_total{{nodename=\"{host}\", mode=\"idle\"}}",
+        # "CPU Idle (s)" : f"node_cpu_seconds_total{{nodename=\"{host}\", mode=\"idle\"}}",
 
         "Total Memory (B)" : f"node_memory_MemTotal_bytes{{nodename=\"{host}\"}}",
         "Available Memory (B)" : f"node_memory_MemAvailable_bytes{{nodename=\"{host}\"}}",
@@ -368,6 +368,10 @@ def extract_node_exporter_data(dashboard_info : dict[str], run_number : int, hos
     rt = ["bytes", "packets", "fifo", "errs", "drop", "compressed"]
     t = ["queue_length", "carrier", "colls"]
     r = ["frame"]
+
+    cpu_times = ["idle", "iowait" "irq", "nice", "softirq", "steal", "system", "user"]
+    for i in cpu_times:
+        query_dict[f"CPU {i} (s)"] = f"node_cpu_seconds_total{{nodename=\"{host}\", mode=\"{i}\"}}"
 
     names = {
         "bytes" : "(Bps)",
