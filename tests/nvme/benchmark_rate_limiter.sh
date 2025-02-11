be4ad5307b460c84fb8ad22bc3ab4f19c31d42f4
Matthew Man <39223700+matthew-man-457@users.noreply.github.com>
2025-02-11 16:26:24 +0100

    Update benchmark_rate_limiter.sh with authorship

--- tests/nvme/benchmark_rate_limiter.sh
+++ tests/nvme/benchmark_rate_limiter.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+###
+# Created on: 2023
+#
+# Author: Matthew Man
+# Affiliation: University of Toronto
+#
+# Description: Benchmarking RAIDs with DUNE DAQ data rates
+###
+
 # need to source the environment file first
 
 if [ $# -ne 6 ]; then
